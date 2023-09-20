f1f02510a0b337ab5a6ce72882025e89bdbd42f8
Alberto89z <20203TN101@utez.edu.mx>
2023-09-19 18:58:01 -0600

    Segundo commit

---
+++ src/config/express.ts
@@ -0,0 +1,24 @@
+import bodyParser from "body-parser";
+import express from "express";
+
+const cors = require('cors');
+const app = express();
+
+app.use(
+    cors({
+        origin: '*'
+    })
+);
+
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
+app.use(express.json({
+    limit: '20mb'
+}))
+
+app.get('/', (req,res) => {
+    res.send('server running asies mi mamawuevo...');
+});
+
+export default app;
